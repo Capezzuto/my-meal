7366d5f579dd91f37f23279ec34b43dc2cb86c2f
Joe Capezzuto <joseph.capezzuto@gmail.com>
2016-08-10 16:04:04 -0700

    [feature] Add google autocomplete to search bar

--- client/index.jsx
+++ client/index.jsx
@@ -4,8 +4,8 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { Router, browserHistory } from 'react-router';
 import thunk from 'redux-thunk';
-
 import { persistStore, autoRehydrate } from 'redux-persist';
+
 import App from './components/app';
 import reducers from './reducers';
 import routes from './routes';
